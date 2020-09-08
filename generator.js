f6888c73dc4c51cd839b9198aba3de2d727f7375
zheta <zheta@microsoft.com>
2020-09-08 22:14:19 +0800

    zhtan: generator

--- generator.js
+++ generator.js
@@ -1,39 +1,57 @@
-
 let resolveHandle = (str) => console.log(str);
-let rejectHandle = () => { throw Error };
+let rejectHandle = () => {
+    throw Error;
+};
 
-let friedSteak = () => new Promise((resolve, reject) => {
-    console.log("start cooking steak");
-    setTimeout(() => resolve("steak done"), 2000);
-}).then(resolveHandle); // callback to chain
+let friedSteak = () =>
+    new Promise((resolve, reject) => {
+        console.log("start cooking steak");
+        setTimeout(() => resolve("steak done"), 2000);
+    });
+let washVegetable = () =>
+    new Promise((resolve, reject) => {
+        console.log("start washing vegetable");
+        setTimeout(() => resolve("washing done"), 500);
+    });
+let cookVegetable = () =>
+    new Promise((resolve, reject) => {
+        console.log("start cooking vegetable");
+        setTimeout(() => resolve("vegetable done"), 1000);
+    });
 
-let washVegetable = () => new Promise((resolve, reject) => {
-    console.log("start washing vegetable");
-    setTimeout(() => resolve("washing done"), 500);
-}).then(resolveHandle);
+let eat = () =>
+    new Promise((resolve, reject) => {
+        console.log("start enjoying dinner");
+        setTimeout(() => resolve("I am full. (¯√¯)"), 10000);
+    });
 
-let cookVegetable = () => new Promise((resolve, reject) => {
-    console.log("start cooking vegetable");
-    setTimeout(() => resolve("vegetable done"), 1000);
-}).then(resolveHandle);
-
-let eat = () => new Promise((resolve, reject) => {
-    console.log("start enjoying dinner");
-    setTimeout(() => resolve("I am full. (¯√¯)"), 10000);
-}).then(resolveHandle);
-
-let dinnerArr = [() => Promise.all([friedSteak(), washVegetable()]), cookVegetable, eat];
+let dinnerArr = [
+    () => Promise.all([friedSteak(), washVegetable()]),
+    cookVegetable,
+    eat,
+];
+// let dinnerArr = [friedSteak, washVegetable, cookVegetable, eat];
 function dinner() {
     function* genDinner() {
         for (let dinnerStep of dinnerArr) {
-            console.log(11);
-            let next = dinnerStep().then((data) => yield data)
-            return next
+            yield dinnerStep();
         }
     }
-    result = genDinner.next();
-    while (result.done) {
-        result.next();
-    }
+    let execuator = genDinner();
+    let getResult = () => {
+        let result = execuator.next();
+
+        // while (!result.done) {
+        //     result.value.then((value) => resolveHandle(value));
+        // }
+        if (result.done) {
+            return;
+        }
+        result.value.then((value) => {
+            resolveHandle(value);
+            getResult();
+        });
+    };
+    getResult();
 }
 dinner();
