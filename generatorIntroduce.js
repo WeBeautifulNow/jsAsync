2bf493cd8834cc82710c68c2b901061aebf071b7
zhtan666 <17621660649@163.com>
2020-09-10 16:18:40 +0800

    add zhexue

--- generatorIntroduce.js
+++ generatorIntroduce.js
@@ -1,8 +1,11 @@
 function* gen(x) {
-    var y = yield x + 2;
+    let y = yield x + 2;
+    console.log('y', y)
     return y;
 }
 
-var g = gen(1);
+let g = gen(1);
 console.log(g.next()); // { value: 3, done: false }
 console.log(g.next()); // { value: undefined, done: true }
+// let firstValue = g.next().value;
+// console.log(g.next(firstValue));
---
+++ ♂/console.js
@@ -0,0 +1,10 @@
+let obj = {
+    a: 1
+};
+console.log(obj.a);
+console.log(obj);
+
+obj.a++;
+
+console.log(obj.a);
+console.log(obj);
