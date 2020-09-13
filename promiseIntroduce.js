3ff7ee17395b70d642c9fa1ed7731b30267befe6
zheta <zheta@microsoft.com>
2020-09-13 19:13:30 +0800

    zhtan: js callback problem

--- promiseIntroduce.js
+++ promiseIntroduce.js
@@ -1,16 +1,21 @@
-var promise = new Promise(function (resolve, reject){
+var promise = new Promise(function (resolve, reject) {
     console.log("inner promise"); // 1
     resolve(42);
     // reject(1);
 });
-promise.then(function(value){
-    console.log(value); // 3
-}, function(value){
-    console.log(`reject for number ${value}`); // 3
-});
+promise.then(
+    function (value) {
+        console.log(value); // 3
+    },
+    function (value) {
+        console.log(`reject for number ${value}`); // 3
+    }
+);
 console.log("outer promise"); // 2
 
-setTimeout(() => {
-    promise.then((value) => console.log(`always async although I got number ${value} already`));
-    console.log("outer promise 2")
-}, 1000)
+//why always async?
+
+// setTimeout(() => {
+//     promise.then((value) => console.log(`always async although I got number ${value} already`));
+//     console.log("outer promise 2")
+// }, 1000)
---
+++ ♂/callbackProblem.js
@@ -0,0 +1,26 @@
+// 不知道被同步调用还是异步调用，难以掌控
+function authentication(callback) {
+    if (!name || !password) {
+        callback();
+        return;
+    }
+    MsPortalFx.ajax("auth", { name, password }, callback);
+}
+
+// 我们把pay的控制权交给第三方paypal.purchase函数，这让我们的回调“任人宰割”
+// 这种情况在早期相对比较常见，比如电商网站，用户用信用卡支付。
+paypal.purchase(purchaseData, function () {
+    pay();
+});
+
+//如果paypal出bug多次调用
+let alreadyPaid = false;
+if (!alreadyPaid) {
+    pay();
+    alreadyPaid = true;
+} else {
+    alert();
+}
+
+//如果调用太慢或者不调用
+//...
